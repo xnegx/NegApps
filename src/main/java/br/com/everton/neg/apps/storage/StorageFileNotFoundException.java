85000874dca293f82746d728f418c634015c7ba7
Neg <Neg@Neg_N>
2017-10-26 23:58:24 -0200

    Adicionado controller de upload e pagna de upload para testes

---
+++ src/main/java/br/com/everton/neg/apps/storage/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package br.com.everton.neg.apps.storage;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
