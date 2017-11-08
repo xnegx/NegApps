c951ff738d187f3d7f1c2ab3815023176e9163ab
Neg <Neg@Neg_N>
2017-11-07 23:08:23 -0200

    Adicionado bootstrap e tela inicial demo

--- src/main/java/br/com/everton/neg/apps/Boot.java
+++ src/main/java/br/com/everton/neg/apps/Boot.java
@@ -1,30 +1,16 @@
 package br.com.everton.neg.apps;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 
-import br.com.everton.neg.apps.storage.StorageProperties;
-import br.com.everton.neg.apps.storage.StorageService;
 
 @SpringBootApplication
-@EnableConfigurationProperties(StorageProperties.class)
 public class Boot
 {
 
    public static void main(String[] args)
    {
       SpringApplication.run(Boot.class, args);
-   }
-   
-   @Bean
-   CommandLineRunner init(StorageService storageService) {
-       return (args) -> {
-           storageService.deleteAll();
-           storageService.init();
-       };
    }
    
 }
